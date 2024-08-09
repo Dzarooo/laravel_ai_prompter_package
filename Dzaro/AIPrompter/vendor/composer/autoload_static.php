2abec0da49fd05dd9fcff9d910e7479468debeb8
Dzarooo <kacperjaroszewski06@gmail.com>
2024-08-09 09:40:42 +0200

    update: removed necessary packages from composer

--- Dzaro/AIPrompter/vendor/composer/autoload_static.php
+++ Dzaro/AIPrompter/vendor/composer/autoload_static.php
@@ -6,38 +6,7 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit5f84b1177b67fade37b18ae68cb61d35
 {
-    public static $files = array (
-        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
-        '61ba3e92ef91a7a0f1cc260ebd9290e4' => __DIR__ . '/..' . '/openai-php/client/src/OpenAI.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Contracts\\Service\\' => 26,
-            'Symfony\\Contracts\\HttpClient\\' => 29,
-            'Symfony\\Component\\HttpClient\\' => 29,
-        ),
-        'P' => 
-        array (
-            'Psr\\Log\\' => 8,
-            'Psr\\Http\\Message\\' => 17,
-            'Psr\\Http\\Client\\' => 16,
-            'Psr\\Container\\' => 14,
-        ),
-        'O' => 
-        array (
-            'OpenAI\\' => 7,
-        ),
-        'N' => 
-        array (
-            'Nyholm\\Psr7\\' => 12,
-        ),
-        'H' => 
-        array (
-            'Http\\Message\\MultipartStream\\' => 29,
-            'Http\\Discovery\\' => 15,
-        ),
         'D' => 
         array (
             'Dzaro\\AIPrompter\\' => 17,
@@ -45,51 +14,6 @@ class ComposerStaticInit5f84b1177b67fade37b18ae68cb61d35
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Contracts\\Service\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/service-contracts',
-        ),
-        'Symfony\\Contracts\\HttpClient\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-client-contracts',
-        ),
-        'Symfony\\Component\\HttpClient\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/http-client',
-        ),
-        'Psr\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/log/src',
-        ),
-        'Psr\\Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'Psr\\Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-client/src',
-        ),
-        'Psr\\Container\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/container/src',
-        ),
-        'OpenAI\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/openai-php/client/src',
-        ),
-        'Nyholm\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/nyholm/psr7/src',
-        ),
-        'Http\\Message\\MultipartStream\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/multipart-stream-builder/src',
-        ),
-        'Http\\Discovery\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/php-http/discovery/src',
-        ),
         'Dzaro\\AIPrompter\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
